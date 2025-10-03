a276a08a7fc55579a14d229fdf2a98667523181f
Mohammad Rifki Indriawan UNP <mohammad.rifki_ti24@nusaputra.ac.id>
2025-10-03 22:40:13 +0700

    initial commit

---
+++ penjumlahanPrima.js
@@ -0,0 +1,16 @@
+let angka = 12
+let tambah = 0
+
+for(i=1;i<=angka;i++){
+    let faktor = 0
+    for(j=1;j<=i;j++){
+        if(i%j===0){
+            faktor++
+        }
+    }
+    if(faktor === 2){
+        console.log(i);
+        tambah += i;
+    }          
+}
+console.log(tambah);
